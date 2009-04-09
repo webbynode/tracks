cdcedaf6924117755c971281752742852abd07cc
Webbynode <felipe@webbynode.com>
2009-04-09 13:58:13 -0300

    Fix database name

--- webby_config.sh
+++ webby_config.sh
@@ -1,4 +1,6 @@
 mv config/site.yml.tmpl config/site.yml
 mv config/database.yml.tmpl config/database.yml
 mkdir log
-sed /salt: "change-me"/salt: "webbynode-salt-change-me"/ config/site.yml
+chown www-data log
+sed s/salt: "change-me"/salt: "webbynode-salt-change-me"/ config/site.yml
+sed s/database: (.*)^/database: $WC_APP_NAME/ config/site.yml
