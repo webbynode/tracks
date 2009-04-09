e5051ccce1aa15cfac69f62937ddb94e80a7417a
Webbynode <felipe@webbynode.com>
2009-04-09 14:18:56 -0300

    Fixing sed

--- webby_config.sh
+++ webby_config.sh
@@ -1,10 +1,10 @@
 sudo gem install RedCloth --no-ri --no-rdoc
 
-mv config/site.yml.tmpl config/site.yml
-mv config/database.yml.tmpl config/database.yml
+cp config/site.yml.tmpl config/site.yml
+cp config/database.yml.tmpl config/database.yml
 
 mkdir log
 chown www-data log
 
-sed s/salt: "change-me"/salt: "webbynode-salt-change-me"/ config/site.yml
-sed s/database: (.*)^/database: $WC_APP_NAME/ config/site.yml
+sed 's/salt: "change-me"/salt: "webbynode-salt-change-me"/' config/site.yml > config/site.yml
+sed 's/database: (.*)^/database: $WC_APP_NAME/' config/database.yml > config/database.yml
