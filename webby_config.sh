01ae7338e8b6e37fe45abc802fb9fa8925ba01ff
Webbynode <felipe@webbynode.com>
2009-04-09 14:21:14 -0300

    Fixed more

--- webby_config.sh
+++ webby_config.sh
@@ -7,4 +7,4 @@ mkdir log
 chown www-data log
 
 sed 's/salt: "change-me"/salt: "webbynode-salt-change-me"/' config/site.yml > config/site.yml
-sed 's/database: (.*)^/database: $WC_APP_NAME/' config/database.yml > config/database.yml
+sed 's/database: (.*)^/database: $WC_APP_NAME/' config/database.yml
